89f6397549ecc3bfce389516c4e9b9d1ff0abf3e
Parth Puri <parth.puri@ril.com>
2023-08-14 19:39:08 +0530

    User profile ui enhancements

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { Avatar, AvatarImage, AvatarFallback } from "@radix-ui/react-avatar";
 import React from "react";
 
@@ -18,12 +19,23 @@ const Profile = () => {
         </div>
 
         <div className="flex flex-col justify-center items-center">
-          <div className="text-lg flex flex-col items-center">Parth Puri</div>
-          <div className="flex gap-4">
-            <div className="text-sm">Following: 34</div>
-            <div className="text-sm">Followers: 30</div>
+          <div className="text-lg flex flex-col items-center font-bold pb-2">
+            Parth Puri
           </div>
-          <div className="text-base text-gray-600 pt-4 w-2/3">
+          <div className="flex gap-4">
+            <div className="text-sm text-gray-500">
+              Following: <span className="text-gray-600">34</span>
+            </div>
+            <div className="text-sm text-gray-500">
+              Followers: <span className="text-gray-600">54</span>
+            </div>
+          </div>
+          <div className="pt-4">
+            <Button className="h-7 rounded-sm bg-sky-400 hover:bg-sky-500 ">
+              Follow
+            </Button>
+          </div>
+          <div className="text-base text-gray-600 pt-4 w-2/3 text-center">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam
             blanditiis laborum deserunt error perferendis culpa nulla aspernatur
             nesciunt, repellendus eligendi?
